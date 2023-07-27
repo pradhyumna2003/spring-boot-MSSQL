e70dd8a987fbb4f26d18519c60935f9356742ac4
pradhyumna_S <pradhyumna.dev@gmail.com>
2023-07-27 12:32:03 +0530

    new updated version

---
+++ src/test/java/com/rqcapi/rqcapp/ApisqlApplicationTests.java
@@ -0,0 +1,14 @@
+package com.rqcapi.rqcapp;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisqlApplicationTests {
+
+	@Test
+	void contextLoads() {	
+	}
+
+}

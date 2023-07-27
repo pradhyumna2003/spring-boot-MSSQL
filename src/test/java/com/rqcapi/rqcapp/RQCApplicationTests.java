19040c6490e87e2c2300ca830e4dd9b1b80d280f
pradhyumna_S <pradhyumna.dev@gmail.com>
2023-07-27 13:56:59 +0530

    app updated 2

--- src/test/java/com/rqcapi/rqcapp/RQCApplicationTests.java
+++ src/test/java/com/rqcapi/rqcapp/RQCApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApisqlApplicationTests {
+class RQCApplicationTests {
 
 	@Test
 	void contextLoads() {	
